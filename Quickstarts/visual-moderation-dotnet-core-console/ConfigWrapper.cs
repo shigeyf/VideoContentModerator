c3ede1a3be78d28d4a51ee045548337ba65a229c
Shige <shigeyf@microsoft.com>
2019-04-13 00:37:11 +0900

    Updated to load JSON output from Azure CLI ams account sp command.

--- Quickstarts/visual-moderation-dotnet-core-console/ConfigWrapper.cs
+++ Quickstarts/visual-moderation-dotnet-core-console/ConfigWrapper.cs
@@ -25,25 +25,25 @@ namespace Microsoft.ContentModerator.VideoContentModerator
     public class VisualModeratorConfig
     {
         private readonly IConfiguration _config;
-        private static string prefix = "VisualModerator:";
+        // private static string prefix = "VisualModerator:";
 
         public VisualModeratorConfig(IConfiguration config)
         {
             this._config = config;
         }
 
-        private string prop(string propertyName) { return prefix + propertyName; }
+        // private string prop(string propertyName) { return prefix + propertyName; }
+        private string prop(string propertyName) { return propertyName; }
 
-        public Uri    AadEndpoint               { get { return new Uri(this._config[prop("AadEndpoint")]); } }
-        public string AadTenantId               { get { return this._config[prop("AadTenantId")]; } }
-        public string AadClientId               { get { return this._config[prop("AadClientId")]; } }
-        public string AadClientSecret           { get { return this._config[prop("AadClientSecret")]; } }
-        public string SubscriptionId            { get { return this._config[prop("SubscriptionId")]; } }
-        public string ResourceGroup             { get { return this._config[prop("ResourceGroup")]; } }
-        public string AccountName               { get { return this._config[prop("AccountName")]; } }
-        public Uri    ArmAadAudience            { get { return new Uri(this._config[prop("ArmAadAudience")]); } }
-        public Uri    ArmEndpoint               { get { return new Uri(this._config[prop("ArmEndpoint")]); } }
-        public bool   EnableStreamingVideo      {  get { return Convert.ToBoolean(this._config[prop("EnableStreamingVideo")]); } }
-        public double StreamingUrlActiveDays    { get { return Convert.ToInt32(this._config[prop("StreamingUrlActiveDays")]); } }
+        public string AadClientId { get { return this._config[prop("AadClientId")]; } }
+        public Uri AadEndpoint { get { return new Uri(this._config[prop("AadEndpoint")]); } }
+        public string AadClientSecret { get { return this._config[prop("AadSecret")]; } }
+        public string AadTenantId { get { return this._config[prop("AadTenantId")]; } }
+        public string AccountName { get { return this._config[prop("AccountName")]; } }
+        public Uri ArmAadAudience { get { return new Uri(this._config[prop("ArmAadAudience")]); } }
+        public Uri ArmEndpoint { get { return new Uri(this._config[prop("ArmEndpoint")]); } }
+        public string ResourceGroup { get { return this._config[prop("ResourceGroup")]; } }
+        public string SubscriptionId { get { return this._config[prop("SubscriptionId")]; } }
+        public bool EnableStreamingVideo { get { return Convert.ToBoolean(this._config[prop("EnableStreamingVideo")]); } }
     }
 }
